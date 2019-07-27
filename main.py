9c6bbcb2cbdd4557c39c81ff299af17fd9bd0fb8
izzudinhafiz <izzudin.hafiz@gmail.com>
2019-07-28 03:07:44 +0800

    Added label_development for temporary dev of label algorithm

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from fyptools import helper_functions as hf
 
 import pandas as pd
 
-
-tickers = hf.get_tickers(1)
-
-for ticker in tickers:
-    main_df = pd.read_csv("fixed_dataset/" + ticker + "_daily_adjusted.csv",
-                          index_col="date", parse_dates=True)
-
-    main_df["decision"] = label.return_label(main_df, 15, offset_type="simple")
-    hf.plot_price_data(main_df, days=300, title="Simple Return Label", ticker=ticker)
+hf.download_time_series(rate=10)
+#
+# tickers = hf.get_tickers(1)
+#
+# for ticker in tickers:
+#     main_df = pd.read_csv("fixed_dataset/" + ticker + "_daily_adjusted.csv",
+#                           index_col="date", parse_dates=True)
+#
+#     main_df["decision"] = label.return_label(main_df, 15, offset_type="simple")
+#     hf.plot_price_data(main_df, days=300, title="Simple Return Label", ticker=ticker)
 
 
