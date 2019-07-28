becddd57f4cffc9811f7e91ddb00e8bb171e7d94
izzudinhafiz <izzudin.hafiz@gmail.com>
2019-07-28 17:22:20 +0800

    label_development for best-fit line running through (xi,yi)

--- label_development.py
+++ label_development.py
@@ -12,23 +12,12 @@ main_df["slope"] = 0
 
 for i, index in enumerate(main_df.index):
     window_df = np.asarray(main_df.loc[main_df.index[i:i+30], "close"])
-    x_data = [x for x in range(len(window_df))]
+    window_df = window_df - window_df[0]
+    x_data = np.asarray([x for x in range(len(window_df))])
 
-    m, c, p, r, std_err = stats.linregress(x_data, window_df)
-    best_line = [x * m + c for x in range(len(x_data))]
-    mid_point = 0
-    if len(best_line) % 2 == 0:
-        # 0,1,2,3,4,5,6,7,8,9
-        mid_index = (len(x_data) - 1) / 2
-        mid_point = (best_line[int(mid_index - 0.5)] + best_line[int(mid_index + 0.5)]) / 2
-        plt.plot(mid_index, mid_point, "rx")
-    else:
-        # 0,1,2,3,4,5,6,7,8
-        mid_index = (len(best_line) - 1) / 2
-        mid_point = best_line[int(mid_index)]
-        plt.plot(mid_index, mid_point, "rx")
-
-    slope = (mid_point - window_df[0]) / mid_index
+    a = np.square(x_data).sum()
+    b = (-2*x_data*window_df).sum()
+    slope = -(b / (2*a))
 
     main_df.loc[index, "slope"] = slope
 
