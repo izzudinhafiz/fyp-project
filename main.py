9af3c3ad4e8d0b73f1d3421290273356f3d2dfb3
izzudinhafiz <izzudin.hafiz@gmail.com>
2019-08-08 02:09:13 +0800

    General update

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-import fyptools.helper_functions as helper
+from fyptools import label_data as label
 import pandas as pd
+from fyptools import backtester
+import fyptools.helper_functions as helper
 
-
-tickers = helper.get_tickers(4, "fixed_dataset/")
-failed_fix = []
-
-for i, ticker in enumerate(tickers):
+tickers = ["AAPL", "A"]
+main_df = {}
+for ticker in tickers:
     ticker_df = helper.read_data(ticker)
-    print("{}/{}".format(i, len(tickers)))
+    ticker_df["decision"] = label.rto_label(ticker_df, offset=15)
+    ticker_df["decision"] = label.remove_lone_decision(ticker_df, cluster_ratio=0.2)
+    ticker_df["decision"] = label.cleanup_region_agreement(ticker_df)
+    main_df[ticker] = ticker_df
 
-    if not isinstance(ticker_df.index, pd.DatetimeIndex):
-        print("Fixing {}".format(ticker))
-        to_remove = []
+main_df = pd.concat(main_df, 1)
 
-        for index in ticker_df.index:
-            if len(index) != 10:
-                to_remove.append(index)
+bt = backtester.Backtester(main_df, 100_000, "1998", "2010")
+decisions = main_df["AAPL", "decision"].loc["1998":"2010"].to_numpy()
 
-        ticker_df.drop(to_remove, inplace=True)
+for i in range(len(bt.dates)-1):
+    if decisions[i] == 1:
+        tp = bt.get_price("AAPL") * 1.1
+        sl = bt.get_price("AAPL") * 0.9
+        bt.portfolio.open_position_value("AAPL", 50000, tp, sl)
+    if decisions[i] == -1:
+        tp = bt.get_price("AAPL") * 0.9
+        sl = bt.get_price("AAPL") * 1.1
+        bt.portfolio.open_position_value("AAPL", -50000, tp, sl)
 
-        try:
-            ticker_df.index = pd.to_datetime(ticker_df.index)
-        except ValueError:
-            print("Failed to fix {}".format(ticker))
-            failed_fix.append(ticker)
-        else:
-            ticker_df.to_csv("fixed_dataset/{}_daily_adjusted.csv".format(ticker))
+    bt.next_day()
 
-if len(failed_fix) != 0:
-    with open("fixed_dataset/failed_fix.csv", "w") as f:
-        if ticker is not failed_fix[-1]:
-            f.write("{},".format(ticker))
-        else:
-            f.write(ticker)
+
+
+bt.end_of_simulation()
+print(bt.history.daily_history)
+summary = bt.history.get_summary()
+profit = sum(summary["true_profit"])
+annualized_return = (bt.portfolio.cash/100_000)**(1/13) - 1
+print(profit + 100_000, bt.portfolio.cash, bt.portfolio.equity)
+print("End of Simulation")
+print("Annualized Return: {:.2f}%".format(bt.portfolio.annualized_return*100))
