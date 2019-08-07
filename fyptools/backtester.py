99e15615cb7ea16c44c8f8df1203cecae04bf648
izzudinhafiz <izzudin.hafiz@gmail.com>
2019-08-08 02:08:45 +0800

    Backtester: - Added equity, cash & positions value attribute - Added daily history attribute - General update

--- fyptools/backtester.py
+++ fyptools/backtester.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 """
 Template for running Backtester
 - Initialize Backtester
@@ -19,6 +20,8 @@ class Backtester(object):
         self.portfolio = Portfolio(self, cash)
         self.history = History(self)
         self.commission_rate = 0.01
+        self.years = int(end) - int(start) + 1
+        self.history.record_daily_history()
 
     def get_price(self, ticker=None):
         return self.data[ticker].loc[self.date, "close"]
@@ -30,11 +33,21 @@ class Backtester(object):
         self.counter += 1
         self.date = self.dates[self.counter]
         self.portfolio.update_positions()
+        self.history.record_daily_history()
 
     def end_of_simulation(self):
+        # Close all positions
         for position in self.portfolio.positions.values():
             position.manually_close("eos")
         self.portfolio.update_positions()
+
+        # Make one last history
+        self.date = self.date + pd.DateOffset(1)
+        self.history.record_daily_history()
+        self.history.make_daily_history()
+
+        # Update records for report
+        self.portfolio.annualized_return = (self.portfolio.equity / self.portfolio.starting_equity)**(1/self.years) - 1
 
     def has_data(self, ticker):
         if str(self.data[ticker].loc[self.date, "close"]) != "nan":
@@ -42,33 +55,56 @@ class Backtester(object):
         else:
             return False
 
+
 class Portfolio(object):
     def __init__(self, backtester, cash):
         self.cash = cash
         self.equity = cash
         self.counter = 0
         self.positions = {}
+        self.long_positions = []
+        self.short_positions = []
         self.to_close = []
         self.bt = backtester
+        self.long_equity = 0
+        self.short_equity = 0
+        self.positions_value = 0
+        self.starting_equity = self.equity
+        self.annualized_return = None
 
     def update_positions(self):
+        self.positions_value = 0
+        self.to_close.clear()
         for identifier, position in self.positions.items():
             position.update_position()
             if position.check_close():
                 position.close()
                 self.to_close.append(identifier)
+            if identifier not in self.to_close:
+                self.positions_value += position.current_value
 
         if len(self.to_close) > 0:
             for identifier in self.to_close:
-                self.cash += self.positions[identifier].true_profit
-                self.bt.history.add(identifier)
+                self.cash += self.positions[identifier].close_value - (self.positions[identifier].nominal_profit
+                                                                       - self.positions[identifier].true_profit)
+                if self.positions[identifier].type == "long":
+                    self.long_positions.remove(identifier)
+                elif self.positions[identifier].type == "short":
+                    self.short_positions.remove(identifier)
+                self.bt.history.record_position(identifier)
                 self.positions.pop(identifier)
-
-        self.to_close.clear()
+        self.equity = self.cash + self.positions_value
 
     def open_position_value(self, ticker, value, tp=None, sl=None):
         self.positions[self.counter] = Position(self.bt, self, self.counter, ticker, value, tp, sl)
+
+        if self.positions[self.counter].type == "long":
+            self.long_positions.append(self.counter)
+        elif self.positions[self.counter].type == "short":
+            self.short_positions.append(self.counter)
+
         self.cash -= value
+        self.positions_value += value
         self.counter += 1
 
     def open_position_unit(self, ticker, unit, tp=None, sl=None):
@@ -106,15 +142,19 @@ class Position(object):
         self.current_value = value
         self.nominal_profit = self.current_value - self.open_value
         self.true_profit = self.nominal_profit - self.open_commission
+        self.daily_change = 0
 
         # To be updated when closed (also true_profit)
-        self.close_commission = 0
+        self.close_commission = None
         self.close_date = None
         self.close_type = None
+        self.close_price = None
+        self.close_value = None
 
     def update_position(self):
         self.current_price = self.bt.get_price(self.ticker)
         self.current_value = self.unit * self.current_price
+        self.daily_change = self.current_value - self.open_value - self.nominal_profit
         self.nominal_profit = self.current_value - self.open_value
         self.true_profit = self.nominal_profit - self.open_commission
 
@@ -122,6 +162,11 @@ class Position(object):
         self.close_commission = abs(self.current_value * self.bt.commission_rate)
         self.close_date = self.bt.get_date()
         self.true_profit = self.nominal_profit - self.open_commission - self.close_commission
+        self.close_price = self.current_price
+        self.close_value = self.current_value
+        self.current_value = None
+        self.current_price = None
+
         return self.id
 
     def check_close(self):
@@ -160,10 +205,11 @@ class Position(object):
 
 class History(object):
     def __init__(self, backtester):
+        self.daily_history = {}
         self.positions = {}
         self.bt = backtester
 
-    def add(self, identifier):
+    def record_position(self, identifier):
         self.positions[identifier] = self.bt.portfolio.positions[identifier]
 
     def get_summary(self):
@@ -224,3 +270,11 @@ class History(object):
                 temp[identifier] = self.get_position_summary_by_id(identifier)
 
         return temp
+
+    def record_daily_history(self):
+        portfolio = self.bt.portfolio
+        daily = {"cash": portfolio.cash, "equity": portfolio.equity, "positions_value": portfolio.positions_value}
+        self.daily_history[self.bt.get_date()] = daily
+
+    def make_daily_history(self):
+        self.daily_history = pd.DataFrame.from_dict(self.daily_history, orient="index")
